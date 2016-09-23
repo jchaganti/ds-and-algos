2d20fd859601f1834db6ffc10bb29c070ee69605
jchaganti <jchaganti@gmail.com>
2016-09-23 14:45:15 +0530

    Use bit vector based implementation

--- cci/src/main/java/org/jch/cci/arraysNStrings/A04PalindromePermutation.java
+++ cci/src/main/java/org/jch/cci/arraysNStrings/A04PalindromePermutation.java
@@ -1,7 +1,5 @@
 package org.jch.cci.arraysNStrings;
 
-import java.util.Arrays;
-
 import com.google.common.base.Strings;
 
 public class A04PalindromePermutation {
@@ -26,15 +24,41 @@ public class A04PalindromePermutation {
 			}
 			return true;
 		}
-		
 		return false;
 	}
+	
+	private boolean isPalindromePermutationUsingBitVector(String input) {
+		if(!Strings.isNullOrEmpty(input)) {
+			int val = 0;
+			char [] chars = input.toCharArray();
+			for(char c: chars) {
+				val = toggleBit(val, (int)c);
+			}
+			// Check if only 1 bit is set to 1
+			return val == 0 || (val &  (val - 1)) == 0;
+		}
+		return false;
+	}
+	
+	
+	private int toggleBit(int val, int c) {
+		int mask = 1 << c;
+		if((val & mask) == 0) {
+			val |= mask;
+		}
+		else {
+			val &= ~mask;
+		}
+		return val;
+	}
+
 	public static void main(String[] args) {
-		String input = "abcdabcdea";
+		String input = "malayalamw";
 		A04PalindromePermutation a04 = new A04PalindromePermutation();
 		boolean res = a04.isPalindromePermutation(input);
-		System.out.println("Res: " + res);
-
+		System.out.println("Res0: " + res);
+		res = a04.isPalindromePermutationUsingBitVector(input);
+		System.out.println("Res1: " + res);
 	}
 
 }
